ac70d386fff1465c3faa530eb2983a8f862aa744
MilesWilliams <miles@klarity.co.za>
2017-05-05 23:45:43 +0200

    Initial Commit

---
+++ Reeder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Reeder
+//
+//  Created by MIles Work on 2017/05/05.
+//  Copyright © 2017 StudioRepublika. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

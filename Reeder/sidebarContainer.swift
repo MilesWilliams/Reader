356585bf2b409c71e6f395788570d483dec6cb54
MilesWilliams <miles@klarity.co.za>
2017-05-07 01:20:24 +0200

    Nearly finishd layout of sidebar nav

---
+++ Reeder/sidebarContainer.swift
@@ -0,0 +1,19 @@
+//
+//  sidebarContainer.swift
+//  Reeder
+//
+//  Created by MIles Work on 2017/05/06.
+//  Copyright © 2017 StudioRepublika. All rights reserved.
+//
+
+import Cocoa
+
+class sidebarContainer: NSScrollView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
